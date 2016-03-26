8b01b97eaa17533fff0b26c23d1aea8dfd492702
Alex Weisberger <alex.m.weisberger@gmail.com>
2016-03-26 15:07:33 -0400

    Fast, clean implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,87 @@
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
 use std::collections::HashMap;
+use std::fs::File;
+use std::path::Path;
+use std::error::Error;
+use std::io::prelude::*;
 
-fn qwerty_to_dvorak_hash() -> HashMap<char, char> {
-    let mut q_to_d = HashMap::new();
-    q_to_d.insert('a', 'a');
-    q_to_d.insert('b', 'x');
-    q_to_d.insert('c', 'j');
-    q_to_d.insert('d', 'e');
-    q_to_d.insert('f', 'u');
-    q_to_d.insert('g', 'i');
-    q_to_d.insert('h', 'd');
-    q_to_d.insert('i', 'c');
-    q_to_d.insert('j', 'h');
-    q_to_d.insert('k', 't');
-    q_to_d.insert('l', 'n');
-    q_to_d.insert('m', 'm');
-    q_to_d.insert('n', 'b');
-    q_to_d.insert('o', 'r');
-    q_to_d.insert('p', 'l');
-    q_to_d.insert('r', 'p');
-    q_to_d.insert('s', 'o');
-    q_to_d.insert('t', 'y');
-    q_to_d.insert('u', 'g');
-    q_to_d.insert('v', 'k');
-    q_to_d.insert('x', 'q');
-    q_to_d.insert('y', 'f');
-    q_to_d
+macro_rules! hash {
+    ( $( $k:expr => $v:expr ),* ) => {
+        {
+            let mut tmp_hash = HashMap::new();
+            $(
+                tmp_hash.insert($k, $v);
+            )*
+            tmp_hash
+        }
+    };
 }
 
-fn convert_words_to_dvorak(s: &str, dvorak_conversion_hash: &HashMap<char, char>) {
-    let full_dict: Vec<&str> = s.split_whitespace().collect();
-    for line in &full_dict {
-        let lower = line.to_lowercase();
-        if lower.contains("e") || lower.contains("q") || lower.contains("w")
-        || lower.contains("z") {
-            continue;
-        }
-
-        let converted = to_dvorak(&lower, dvorak_conversion_hash);
-        let needle: &str = &converted;
-        if full_dict.contains(&needle) {
-            println!("qwerty: {} | dvorak: {}", lower, converted)
-        }
-    }
+fn qd_map() -> HashMap<char, char> {
+    hash!(
+        'a' => 'a', 'A' => 'A', 'b' => 'x', 'B' => 'X',    
+        'c' => 'j', 'C' => 'J', 'd' => 'e', 'D' => 'E',    
+        'f' => 'u', 'F' => 'U', 'g' => 'i', 'G' => 'I',    
+        'h' => 'd', 'H' => 'D', 'i' => 'c', 'I' => 'C',    
+        'j' => 'h', 'J' => 'H', 'k' => 't', 'K' => 'T',
+        'l' => 'n', 'L' => 'N', 'm' => 'm', 'M' => 'M',    
+        'n' => 'b', 'N' => 'B', 'o' => 'r', 'O' => 'R',    
+        'p' => 'l', 'P' => 'L', 'r' => 'p', 'R' => 'P',    
+        's' => 'o', 'S' => 'O', 't' => 'y', 'T' => 'Y',    
+        'u' => 'g', 'U' => 'G', 'v' => 'k', 'V' => 'K',    
+        'x' => 'q', 'X' => 'Q', 'y' => 'f', 'Y' => 'F'
+     )
 }
 
-fn to_dvorak(qwerty: &str, conversion_hash: &HashMap<char, char>) -> String {
-    let mut converted = String::from("");
-    for c in String::from(qwerty).chars() {
-        match conversion_hash.get(&c) {
-            Some(dvorak) => converted.push(*dvorak),
-            None => panic!("no conversion for {}", c)
+fn open_dict() -> File {
+    let path = Path::new("/usr/share/dict/words");
+    let display = path.display();
+    
+    let file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display,
+                                                   Error::description(&why)),
+        Ok(file) => file
+    };
+    file
+}
+
+fn parse_words<'a>(dict_file: &mut File, dict_contents: &'a mut String) -> Vec<&'a str> {
+    // Use str.lines() ?
+    let words = match dict_file.read_to_string(dict_contents) {
+        Ok(_) => {
+            dict_contents.split_whitespace().collect::<Vec<&'a str>>()
         }
-    }
-    converted
+        Err(_) => panic!("couldn't parse dict file")
+    };
+    words
 }
 
 fn main() {
-    let path = Path::new("/usr/share/dict/words");
-    let display = path.display();
+    let qd_map = qd_map();
 
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display,
-                                                   Error::description(&why)),
-        Ok(file) => file,
-    };
-
+    // Parse dict
+    let mut dict_file = open_dict();
     let mut s = String::new();
-    match file.read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read {}: {}", display,
-                                                   Error::description(&why)),
-        Ok(_) => {
-            let q_to_d = qwerty_to_dvorak_hash();
-            convert_words_to_dvorak(&s, &q_to_d)
-        }
+    let words = parse_words(&mut dict_file, &mut s);
+
+    let valid_words: Vec<&&str> = words.iter().filter(|word| 
+        !word.contains("q") && !word.contains("Q") &&
+        !word.contains("w") && !word.contains("W") &&
+        !word.contains("e") && !word.contains("E") && 
+        !word.contains("z") && !word.contains("Z")
+    ).collect();
+
+    // create index
+    let mut index: HashMap<String, bool> = HashMap::new();  
+    for word in &words {
+        index.insert(String::from(*word), true);
+    }
+
+    // Convert and check
+    for word in valid_words {
+        let converted = word.chars().map(|c| qd_map[&c]).collect::<String>();
+        match index.get(&converted) {
+            Some(_) => println!("{} -> {}", word, converted),
+            None    => {}
+        };
     }
 }
